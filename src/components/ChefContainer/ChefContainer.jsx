be7a013cd5c287fa4a7fbb555a37738994104373
emonh0que <you@example.com>
2023-05-02 18:56:50 +0600

    added chef card

--- src/components/ChefContainer/ChefContainer.jsx
+++ src/components/ChefContainer/ChefContainer.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SingleChef from '../SingleChef/SingleChef';
 
 const ChefContainer = () => {
+    const [chefs, setChefs] = useState([]);
+
+    useEffect(() => {
+        fetch('chef.json')
+        .then(res => res.json())
+        .then(data => setChefs(data.chefs))
+    }, [])
     return (
-        <div className='p-4 mt-[60px]'>
-            <SingleChef></SingleChef>
+        <div className='p-4 mt-[60px] grid grid-cols-3 gap-3'>
+            {
+                chefs.map(chef => <SingleChef key={chef.id} chef={chef}></SingleChef>)
+            }
         </div>
     );
 };
--- src/components/SingleChef/SingleChef.jsx
+++ src/components/SingleChef/SingleChef.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { FaRegHeart } from 'react-icons/fa';
-const SingleChef = () => {
+const SingleChef = ({chef}) => {
+    const {chef_picture, chef_name, years_of_experience, number_of_recipes, likes } = chef;
     return (
-        <div className='flex flex-col items-center justify-center gap-8'>
-                <img className='w-[150px] rounded-[50%] h-[150px]' src="https://img.freepik.com/free-photo/young-confident-caucasian-cook-girl-chef-uniform-stands-sideways-holds-rolling-pin-isolated-violet-wall-with-copy-space_141793-33236.jpg?w=740&t=st=1683029647~exp=1683030247~hmac=259ce615ffc89f5ff30be8b3c503caf6d4a6861aae6db8060bc23e3e882ade25" alt="" />
-                <h2 className='text-2xl font-semibold'>Soha Sen Gupta</h2>
+        <div className='flex flex-col items-center bg-[#538017] rounded-xl justify-center gap-8'>
+                <img className='w-[150px] rounded-[50%] h-[150px]' src={chef_picture} alt="" />
+                <h2 className='text-2xl font-semibold'>{chef_name}</h2>
                 <div className='flex bg-[#FE8E78] rounded-xl px-6 py-4 justify-between items-center gap-4'>
-                    <p className='text-sm  font-medium'>3+ years of experience</p>
-                    <p className='text-sm font-medium'>4 Recipes</p>
-                    <p className='text-sm flex gap-2 items-center font-medium'> <FaRegHeart className='text-red-500' /> 7 Like</p>
+                    <p className='text-sm  font-medium'>{years_of_experience} years of experience</p>
+                    <p className='text-sm font-medium'>{number_of_recipes} Recipes</p>
+                    <p className='text-sm flex gap-2 items-center font-medium'> <FaRegHeart className='text-red-500' /> {likes} Like</p>
                 </div>
                 <button className="font-medium bg-[#3A1C36] text-white border px-6 py-4 rounded-lg">View Recipies</button>
             </div>
