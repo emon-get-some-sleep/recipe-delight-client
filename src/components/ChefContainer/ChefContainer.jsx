31849ac464f4082611ce9c5e1836956112ae8d20
emonh0que <you@example.com>
2023-05-02 18:35:00 +0600

    created the first chef card

---
+++ src/components/ChefContainer/ChefContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SingleChef from '../SingleChef/SingleChef';
+
+const ChefContainer = () => {
+    return (
+        <div className='p-4 mt-[60px]'>
+            <SingleChef></SingleChef>
+        </div>
+    );
+};
+
+export default ChefContainer;
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import FoodBanner from '../FoodBanner/FoodBanner';
+import ChefContainer from '../ChefContainer/ChefContainer';
 
 const HomePage = () => {
     return (
         <div>
             <FoodBanner></FoodBanner>
+            <ChefContainer></ChefContainer>
         </div>
     );
 };
